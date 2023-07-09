2f06d96b42f8c3ca71a59be4a272780008a02eb9
Shahar Vaknin <shaharvaknin909@gamil.com0>
2023-07-09 15:59:54 +0300

    add router and react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Posts } from "./components/posts";
 import { Users } from "./components/users";
 
 const queryClient = new QueryClient();
@@ -8,8 +9,10 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      {" "}
-      <Users />
+      <Routes>
+        <Route path="/" element={<Users />} />
+        <Route path="/posts/:userId" element={<Posts />} />
+      </Routes>
     </QueryClientProvider>
   );
 }
